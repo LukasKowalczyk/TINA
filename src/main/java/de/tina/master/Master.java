ccaf5fbf20eca78ca23039ee64a8bec8b124f1ed
LukasKowalczyk <kowalczyk.lukas.1987@gmail.com>
2018-03-12 21:26:33 +0100

    Refactoring

--- src/main/java/de/tina/master/Master.java
+++ src/main/java/de/tina/master/Master.java
@@ -11,7 +11,6 @@ import java.util.Map;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.RandomStringUtils;
-import org.apache.commons.lang.StringUtils;
 
 import de.tina.knowledge.Analyser;
 import de.tina.knowledge.KnowledgeBase;
