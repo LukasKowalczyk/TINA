d9e82548a5218773bf6166108349255dc8fb5c8d
LukasKowalczyk <kowalczyk.lukas.1987@gmail.com>
2018-03-14 19:18:54 +0100

    SpringBoot

---
+++ src/main/java/de/tina/TINA.java
@@ -0,0 +1,12 @@
+package de.tina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class TINA {
+	public static void main(String[] args) {
+		SpringApplication.run(TINA.class, args);
+	}
+}
---
+++ src/test/java/de/tina/TinaTests.java
@@ -0,0 +1,16 @@
+package de.tina;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TinaTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
