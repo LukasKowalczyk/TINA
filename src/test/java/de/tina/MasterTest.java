5344dfee1d7e0bd0a04da4af7f5e7ad6203bdb98
Lukas Kowalczyk <kowalczyk.lukas.1987@gmail.com>
2018-03-14 15:32:30 +0100

    Master Junit-test

--- src/test/java/de/tina/MasterTest.java
+++ src/test/java/de/tina/MasterTest.java
@@ -1,24 +1,44 @@
 package de.tina;
 
-import static org.junit.Assert.*;
-
+import java.io.File;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import de.tina.apprentice.Apprentice;
+import de.tina.knowledge.JsonFilenameFilter;
+import de.tina.master.Master;
 
 public class MasterTest {
+    private static final int HIT_QUOTA = 90;
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
+    private static final boolean PRE_FILTER = false;
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
+    private static final String TEST_SOURCE_PATH = "C:\\temp\\";
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        for (File file : new File(TEST_SOURCE_PATH).listFiles(new JsonFilenameFilter())) {
+            file.delete();
+        }
+        Apprentice apprentice = new Apprentice(TEST_SOURCE_PATH);
+        apprentice.learn("Hallo wie geht es dir?", "Begrüßung");
+        apprentice.learn("Geht es dir gut?", "Begrüßung");
+        apprentice.learn("Hallo, ich kündige.", "Kündigung");
+        apprentice.learn("Hallo, ich möchte kündigen.", "Kündigung");
+        apprentice.finish();
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+        for (File file : new File(TEST_SOURCE_PATH).listFiles(new JsonFilenameFilter())) {
+            file.delete();
+        }
+    }
+
+    @Test
+    public void testKuendigung() {
+        Master master = new Master(TEST_SOURCE_PATH, PRE_FILTER, HIT_QUOTA);
+        master.ask("Hallo, ich kündige.");
+    }
 
 }
