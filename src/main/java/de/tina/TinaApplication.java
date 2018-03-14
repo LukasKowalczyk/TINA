7cdbf95626fd68e524ec8b7244a63870c6e88b62
LukasKowalczyk <kowalczyk.lukas.1987@gmail.com>
2018-03-14 20:13:50 +0100

    SpringBoot

--- src/main/java/de/tina/TinaApplication.java
+++ src/main/java/de/tina/TinaApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
 public class TinaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TinaApplication.class, args);
